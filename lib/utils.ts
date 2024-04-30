57f0121273823ecce1b7fe59cb5d54f390fd1ad6
Olumide Bambe <loyalsfc@gmail.com>
2024-04-30 09:07:34 +0100

    working on updating profile images

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import {Cloudinary} from "@cloudinary/url-gen";
+import { env } from "process";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const cld = new Cloudinary({cloud: {cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_NAME}});
